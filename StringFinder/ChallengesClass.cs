19f9a6f0b3856ee277a79aabf0b14b5f53f61443
Elfin <mr.hawl1227@gmail.com>
2022-10-21 16:22:35 +0300

    102120221622P

---
+++ StringFinder/ChallengesClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFinder
+{
+    public static class ChallengesClass
+    {
+        public static bool CanFind(string[] userInputs, string[] words)
+        {
+            if (userInputs != null && words != null)
+            {
+                bool isSuccess = false;
+                for (int i = 0; i < words.Length; i++)
+                {
+                    foreach (var input in userInputs)
+                    {
+                        bool isExist = words[i].Contains(input);
+
+                        if (isExist)
+                        {
+                            isSuccess = true;
+                        }
+                        else
+                        {
+                            isSuccess = false;
+                        }
+                    }
+                }
+
+                return isSuccess;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ StringFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿using StringFinder;
+
+Console.WriteLine("Please enter the number of inputs that you want :");
+int numberOfInputs = int.Parse(Console.ReadLine());
+
+List<string> userInputs = new List<string>();
+List<string> userWords = new List<string>();
+
+for (int i = 0; i < numberOfInputs; i++)
+{
+    Console.WriteLine("Please enter input {0} :", i);
+    string inputText = Console.ReadLine();
+
+    if (inputText != null && inputText.Length <= 2)
+    {
+        userInputs.Add(inputText);
+    }
+}
+
+Console.WriteLine("Now enter your words to check :");
+
+for (int i = 0; i < numberOfInputs; i++)
+{
+    Console.WriteLine("Please enter word {0} :", i);
+    string wordText = Console.ReadLine();
+
+    if (wordText != null)
+    {
+        userWords.Add(wordText);
+    }
+}
+
+Console.WriteLine(ChallengesClass.CanFind(userInputs.ToArray(), userWords.ToArray()));
